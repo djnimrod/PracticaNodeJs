7379f4b5657d8e5d89206b20990f78f25aaac5c6
djnimrod <valcyum.085@gmail.com>
2022-05-24 00:21:27 -0400

    clases, exports, metodos librerias

---
+++ app.js
@@ -0,0 +1,7 @@
+const sumar = require('./sumar');
+const multiplica = require('./multiplica');
+
+console.log(sumar.sumarDosMasDos());
+console.log(sumar.suma(6,6));
+
+console.log(multiplica.multiplica(4,10));
---
+++ os.js
@@ -0,0 +1,5 @@
+const os = require('os');
+
+console.log('version SO ',os.release());
+
+console.log('memoria libre: ',os.freemem());
